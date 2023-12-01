50cc00035b635a3f27ad61c084e69a667e865a6d
Konrad Malik <konrad.malik@gmail.com>
2023-12-01 16:10:54 +0100

    feat: run actual commands on events

--- src/handling.rs
+++ src/handling.rs
@@ -1,3 +1,5 @@
+use std::process::{Command, Stdio};
+
 use crate::watching::ChangeEvent;
 
 pub trait EventHandler {
@@ -17,5 +19,20 @@ impl CommandEventHandler {
 impl EventHandler for CommandEventHandler {
     fn handle(&self, event: ChangeEvent) {
         log::debug!("{event}, command: {:?}", self.command);
+
+        // FIXME this does not respect shell aliases, should it?
+        let mut cmd = Command::new(
+            self.command
+                .first()
+                .expect("command did not contain any elements"),
+        )
+        .args(&self.command[1..])
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+
+        let status = cmd.wait().unwrap();
+        println!("{status}")
     }
 }
